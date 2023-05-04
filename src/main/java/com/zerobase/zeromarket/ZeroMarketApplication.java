65f7b8010f3943402c495f0743f85bd862cbad14
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-05-04 23:49:21 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/zeromarket/ZeroMarketApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.zeromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/zeromarket/ZeroMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.zeromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
